9acf26178fc0c588ab3851ab3a543c03a690a6ac
Rhys Arkins <rhys@arkins.net>
2025-03-03 11:26:35 +0100

    build: add copy.sh script

---
+++ bin/copy.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Error: No release argument provided."
+  exit 1
+fi
+
+RELEASE=$1
+
+appdockerfile=tmp/agent-4-github-enterprise-$RELEASE/wss-ghe-app/docker/Dockerfile
+
+if [ ! -f $appdockerfile ]; then
+  echo "Error: $appdockerfile not found."
+  exit 1
+fi
+
+sed '/# END OF BASE IMAGE/ q' $appdockerfile > repo-integrations/controller/Dockerfile
+
+scaScannerDockerfile=tmp/agent-4-github-enterprise-$RELEASE/wss-scanner/docker/Dockerfile
+
+if [ ! -f $scaScannerDockerfile ]; then
+  echo "Error: $scaScannerDockerfile not found."
+  exit 1
+fi
+
+sed '/# END OF BASE IMAGE/ q' $scaScannerDockerfile > repo-integrations/scanner/Dockerfile
+
+scaScannerDockerfilefull=tmp/agent-4-github-enterprise-$RELEASE/wss-scanner/docker/Dockerfilefull
+
+if [ ! -f $scaScannerDockerfilefull ]; then
+  echo "Error: $scaScannerDockerfilefull not found."
+  exit 1
+fi
+
+sed '/# END OF BASE IMAGE/ q' $scaScannerDockerfilefull > repo-integrations/scanner/Dockerfilefull
+
+remediateDockerfile=tmp/agent-4-github-enterprise-$RELEASE/wss-remediate/docker/Dockerfile
+
+if [ ! -f $remediateDockerfile ]; then
+  echo "Error: $remediateDockerfile not found."
+  exit 1
+fi
+
+sed '/# END OF BASE IMAGE/ q' $remediateDockerfile > repo-integrations/remediate/Dockerfile
