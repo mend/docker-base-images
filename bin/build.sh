8002ea33c6f3745e028fe03e860ccf9e02c3df07
Rhys Arkins <rhys@arkins.net>
2025-03-24 17:46:08 +0100

    build: no-cache

--- bin/build.sh
+++ bin/build.sh
@@ -8,7 +8,7 @@ fi
 RELEASE=$1
 
 docker pull ubuntu:24.04
-docker build -t mend/base-repo-controller:${RELEASE} -f repo-integrations/controller/Dockerfile .
-docker build -t mend/base-repo-remediate:${RELEASE} -f repo-integrations/remediate/Dockerfile .
-docker build -t mend/base-repo-scanner:${RELEASE} -f repo-integrations/scanner/Dockerfile .
-docker build -t mend/base-repo-scanner:${RELEASE}-full -f repo-integrations/scanner/Dockerfile.full .
+docker build --no-cache -t mend/base-repo-controller:${RELEASE} -f repo-integrations/controller/Dockerfile .
+docker build --no-cache -t mend/base-repo-remediate:${RELEASE} -f repo-integrations/remediate/Dockerfile .
+docker build --no-cache -t mend/base-repo-scanner:${RELEASE} -f repo-integrations/scanner/Dockerfile .
+docker build --no-cache -t mend/base-repo-scanner:${RELEASE}-full -f repo-integrations/scanner/Dockerfile.full .
