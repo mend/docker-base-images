a4f81a08458c52406d362b5b7ca1e1527947e027
nadav-t <nadav.tzabari@mend.io>
2025-11-13 00:36:14 +0200

    fix ECR publish fix image push by tag

--- bin/stg-publish.sh
+++ bin/stg-publish.sh
@@ -9,12 +9,20 @@ fi
 if [ "$1" = "1.1.1" ]; then
   echo "Error: Default version tag provided. Please provide the correct tag"
   exit 1
-fi 
+fi
 
 RELEASE=$1
 
-docker push $ECR_REGISTRY/$ECR_REPOSITORY:base-repo-controller:${RELEASE}
-docker push $ECR_REGISTRY/$ECR_REPOSITORY:base-repo-remediate:${RELEASE}
-docker push $ECR_REGISTRY/$ECR_REPOSITORY:base-repo-scanner:${RELEASE}
-docker push $ECR_REGISTRY/$ECR_REPOSITORY:base-repo-scanner:${RELEASE}-full
-docker push $ECR_REGISTRY/$ECR_REPOSITORY:base-repo-scanner-sast:${RELEASE}
+# Tag all images for ECR
+docker tag mend/base-repo-controller:${RELEASE} $ECR_REGISTRY/$ECR_REPOSITORY/base-repo-controller:${RELEASE}
+docker tag mend/base-repo-remediate:${RELEASE} $ECR_REGISTRY/$ECR_REPOSITORY/base-repo-remediate:${RELEASE}
+docker tag mend/base-repo-scanner:${RELEASE} $ECR_REGISTRY/$ECR_REPOSITORY/base-repo-scanner:${RELEASE}
+docker tag mend/base-repo-scanner:${RELEASE}-full $ECR_REGISTRY/$ECR_REPOSITORY/base-repo-scanner:${RELEASE}-full
+docker tag mend/base-repo-scanner-sast:${RELEASE} $ECR_REGISTRY/$ECR_REPOSITORY/base-repo-scanner-sast:${RELEASE}
+
+# Push all images to ECR
+docker push $ECR_REGISTRY/$ECR_REPOSITORY/base-repo-controller:${RELEASE}
+docker push $ECR_REGISTRY/$ECR_REPOSITORY/base-repo-remediate:${RELEASE}
+docker push $ECR_REGISTRY/$ECR_REPOSITORY/base-repo-scanner:${RELEASE}
+docker push $ECR_REGISTRY/$ECR_REPOSITORY/base-repo-scanner:${RELEASE}-full
+docker push $ECR_REGISTRY/$ECR_REPOSITORY/base-repo-scanner-sast:${RELEASE}
