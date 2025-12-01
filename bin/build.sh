630187528b4cdd01f5771b3c276907931c9ed52e
nadav-t <nadav.tzabari@mend.io>
2025-12-01 13:23:03 +0200

    ECR_REGISTRY hardcoded

--- bin/build.sh
+++ bin/build.sh
@@ -5,6 +5,9 @@ RELEASE=$1
 REGISTRY_PREFIX=$2
 COPY_VERSIONS_JSON=$3
 
+# Hardcoded ECR registry
+ECR_REGISTRY="054331651301.dkr.ecr.us-east-1.amazonaws.com"
+
 if [ -z "$RELEASE" ]; then
   echo "Error: No release argument provided."
   echo "Usage: $0 <release> <registry_prefix>"
@@ -31,41 +34,47 @@ echo "Building images with registry prefix: ${REGISTRY_PREFIX}"
 docker pull ubuntu:24.04
 
 
-docker build --no-cache -t ${REGISTRY_PREFIX}/base-repo-scanner-sast:${RELEASE} -f repo-integrations/scanner/DockerfileSast .
-docker build --no-cache -t ${REGISTRY_PREFIX}/base-repo-controller:${RELEASE} -f repo-integrations/controller/Dockerfile .
-docker build --no-cache -t ${REGISTRY_PREFIX}/base-repo-remediate:${RELEASE} -f repo-integrations/remediate/Dockerfile .
-docker build --no-cache -t ${REGISTRY_PREFIX}/base-repo-scanner:${RELEASE} -f repo-integrations/scanner/Dockerfile .
-docker build --no-cache -t ${REGISTRY_PREFIX}/base-repo-scanner:${RELEASE}-full -f repo-integrations/scanner/Dockerfile.full .
+#docker build --no-cache -t ${REGISTRY_PREFIX}/base-repo-scanner-sast:${RELEASE} -f repo-integrations/scanner/DockerfileSast .
+#docker build --no-cache -t ${REGISTRY_PREFIX}/base-repo-controller:${RELEASE} -f repo-integrations/controller/Dockerfile .
+#docker build --no-cache -t ${REGISTRY_PREFIX}/base-repo-remediate:${RELEASE} -f repo-integrations/remediate/Dockerfile .
+#docker build --no-cache -t ${REGISTRY_PREFIX}/base-repo-scanner:${RELEASE} -f repo-integrations/scanner/Dockerfile .
+#docker build --no-cache -t ${REGISTRY_PREFIX}/base-repo-scanner:${RELEASE}-full -f repo-integrations/scanner/Dockerfile.full .
 
-#Validate built images successfully created
-echo "🔍 Validating built images..."
-if [ -z "$(docker images -q ${REGISTRY_PREFIX}/base-repo-scanner-sast:${RELEASE} 2> /dev/null)" ]; then
-  echo "❌ ${REGISTRY_PREFIX}/base-repo-scanner-sast:${RELEASE} was not built successfully"
-  exit 1
-fi
-echo "✅ SAST scanner image validated"
+docker build --no-cache -t ${ECR_REGISTRY}/base-repo-scanner-sast:${RELEASE} -f repo-integrations/scanner/DockerfileSast .
+docker build --no-cache -t ${ECR_REGISTRY}/base-repo-controller:${RELEASE} -f repo-integrations/controller/Dockerfile .
+docker build --no-cache -t ${ECR_REGISTRY}/base-repo-remediate:${RELEASE} -f repo-integrations/remediate/Dockerfile .
+docker build --no-cache -t ${ECR_REGISTRY}/base-repo-scanner:${RELEASE} -f repo-integrations/scanner/Dockerfile .
+docker build --no-cache -t ${ECR_REGISTRY}/base-repo-scanner:${RELEASE}-full -f repo-integrations/scanner/Dockerfile.full .
+#
+##Validate built images successfully created
+#echo "🔍 Validating built images..."
+#if [ -z "$(docker images -q ${REGISTRY_PREFIX}/base-repo-scanner-sast:${RELEASE} 2> /dev/null)" ]; then
+#  echo "❌ ${REGISTRY_PREFIX}/base-repo-scanner-sast:${RELEASE} was not built successfully"
+#  exit 1
+#fi
+#echo "✅ SAST scanner image validated"
+#
+#if [ -z "$(docker images -q ${REGISTRY_PREFIX}/base-repo-controller:${RELEASE} 2> /dev/null)" ]; then
+#  echo "❌ ${REGISTRY_PREFIX}/base-repo-controller:${RELEASE} was not built successfully"
+#  exit 1
+#fi
+#echo "✅ Controller image validated"
+#
+#if [ -z "$(docker images -q ${REGISTRY_PREFIX}/base-repo-remediate:${RELEASE} 2> /dev/null)" ]; then
+#  echo "❌ ${REGISTRY_PREFIX}/base-repo-remediate:${RELEASE} was not built successfully"
+#  exit 1
+#fi
+#echo "✅ Remediate image validated"
+#
+#if [ -z "$(docker images -q ${REGISTRY_PREFIX}/base-repo-scanner:${RELEASE} 2> /dev/null)" ]; then
+#  echo "❌ ${REGISTRY_PREFIX}/base-repo-scanner:${RELEASE} was not built successfully"
+#  exit 1
+#fi
+#echo "✅ SCA scanner image validated"
+#
+#if [ -z "$(docker images -q ${REGISTRY_PREFIX}/base-repo-scanner:${RELEASE}-full 2> /dev/null)" ]; then
+#  echo "❌ ${REGISTRY_PREFIX}/base-repo-scanner:${RELEASE}-full was not built successfully"
+#  exit 1
+#fi
 
-if [ -z "$(docker images -q ${REGISTRY_PREFIX}/base-repo-controller:${RELEASE} 2> /dev/null)" ]; then
-  echo "❌ ${REGISTRY_PREFIX}/base-repo-controller:${RELEASE} was not built successfully"
-  exit 1
-fi
-echo "✅ Controller image validated"
-
-if [ -z "$(docker images -q ${REGISTRY_PREFIX}/base-repo-remediate:${RELEASE} 2> /dev/null)" ]; then
-  echo "❌ ${REGISTRY_PREFIX}/base-repo-remediate:${RELEASE} was not built successfully"
-  exit 1
-fi
-echo "✅ Remediate image validated"
-
-if [ -z "$(docker images -q ${REGISTRY_PREFIX}/base-repo-scanner:${RELEASE} 2> /dev/null)" ]; then
-  echo "❌ ${REGISTRY_PREFIX}/base-repo-scanner:${RELEASE} was not built successfully"
-  exit 1
-fi
-echo "✅ SCA scanner image validated"
-
-if [ -z "$(docker images -q ${REGISTRY_PREFIX}/base-repo-scanner:${RELEASE}-full 2> /dev/null)" ]; then
-  echo "❌ ${REGISTRY_PREFIX}/base-repo-scanner:${RELEASE}-full was not built successfully"
-  exit 1
-fi
-
-echo "🎉 All images built successfully with prefix: ${REGISTRY_PREFIX}"
+echo "🎉 All images built successfully with prefix: ${ECR_REGISTRY}"
